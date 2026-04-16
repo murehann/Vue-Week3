f373909d4d82ad9ec688b49e90294974baf37bbb
Muhammad Rehan <murehan@nisum.com>
2026-04-16 11:32:38 +0500

    PRODUCT: TESTING - add unit test for wishslists store

--- product-app/src/_tests_/stores/wishlists.spec.js
+++ product-app/src/_tests_/stores/wishlists.spec.js
@@ -2,36 +2,91 @@ import { useWishlistStore } from '@/stores/wishlists'
 import { setActivePinia, createPinia } from 'pinia'
 import { beforeEach, describe, expect, test } from 'vitest'
 
-test('sample', () => {
-  expect(2).toBe(2)
-})
-
 describe('Wishlists Store', () => {
-  beforeEach(() => {
-    setActivePinia(createPinia())
-  })
-
   const testData = {
     id: 1,
     title: 'Title',
     imageLink: 'link',
   }
 
-  test('adds product to wishlist', () => {
-    const store = useWishlistStore()
-    expect(store.products.length).toBe(0)
+  const testData2 = {
+    id: 2,
+    title: 'Title2',
+    imageLink: 'link2',
+  }
 
-    store.addToWishList(testData.id, testData.title, testData.imageLink)
-
-    expect(store.products.length).toBe(1)
-    expect(store.products[0]).toEqual(testData)
+  beforeEach(() => {
+    setActivePinia(createPinia())
   })
 
-  test('doesnt add product to wihslist if already in wishlist', () => {
-    const store = useWishlistStore()
+  describe('isWishlisted', () => {
+    test('returns true if product is in wishlist', () => {
+      const store = useWishlistStore()
+      store.$patch({ products: [testData] })
 
-    store.addToWishList(testData.id, testData.title, testData.imageLink)
-    store.addToWishList(testData.id, testData.title, testData.imageLink)
-    expect(store.products.length).toBe(1)
+      expect(store.isWishListed(testData.id)).toBe(true)
+    })
+
+    test('returns false if product is not in wishlist', () => {
+      const store = useWishlistStore()
+      store.$patch({ products: [testData2] })
+
+      expect(store.isWishListed(testData.id)).toBe(false)
+    })
+  })
+
+  describe('addToWishlist', () => {
+    test('adds product to wishlist', () => {
+      const store = useWishlistStore()
+
+      store.addToWishList(testData.id, testData.title, testData.imageLink)
+
+      expect(store.products.length).toBe(1)
+      expect(store.products[0]).toEqual(testData)
+    })
+
+    test('adds multiple products to wishlist', () => {
+      const store = useWishlistStore()
+
+      store.addToWishList(testData.id, testData.title, testData.imageLink)
+      store.addToWishList(testData2.id, testData2.title, testData2.imageLink)
+
+      expect(store.products.length).toBe(2)
+      expect(store.products[0]).toEqual(testData)
+      expect(store.products[1]).toEqual(testData2)
+    })
+
+    test('doesnt add product to wishlist if already in wishlist', () => {
+      const store = useWishlistStore()
+
+      store.addToWishList(testData.id, testData.title, testData.imageLink)
+      store.addToWishList(testData.id, testData.title, testData.imageLink)
+
+      expect(store.products.length).toBe(1)
+      expect(store.products[0]).toEqual(testData)
+    })
+  })
+
+  describe('removeFromWishlist', () => {
+    test('removes correct product if exists in wishlist', () => {
+      const store = useWishlistStore()
+      store.$patch({ products: [testData, testData2] })
+
+      store.removeFromWishList(testData.id)
+
+      expect(store.products.length).toBe(1)
+      expect(store.isWishListed(testData.id)).toBe(false)
+      expect(store.isWishListed(testData2.id)).toBe(true)
+    })
+
+    test('does nothing if product not in wishlist', () => {
+      const store = useWishlistStore()
+      store.$patch({ products: [testData, testData2] })
+
+      store.removeFromWishList(testData.id + testData2.id)
+
+      expect(store.isWishListed(testData.id)).toBe(true)
+      expect(store.isWishListed(testData2.id)).toBe(true)
+    })
   })
 })
