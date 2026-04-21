668706d7018da135bc501220dfd63fee7e5927b4
Muhammad Rehan <murehan@nisum.com>
2026-04-21 12:26:22 +0500

    PRODUCT: Add carousel functionality to ProductImageList

--- product-app/src/components/ProductImageList.vue
+++ product-app/src/components/ProductImageList.vue
@@ -1,7 +1,8 @@
 <script setup>
+import { computed, onMounted, ref } from 'vue'
 import ProductImage from './ProductImage.vue'
 
-defineProps({
+const props = defineProps({
   images: {
     type: Array,
     required: true,
@@ -15,21 +16,75 @@ defineProps({
     required: true,
   },
 })
+
+const currentImgIndex = ref(0)
+
+const slideWidth = props.imageDimensions.width * 4
+
+const containerRef = ref(null)
+const containerWidth = ref(0)
+
+const centerOffset = computed(() => {
+  return (containerWidth.value - (slideWidth + 32 * (currentImgIndex.value + 1))) / 2
+}) // 32 = p-4 + gap-4, times i + 1 for each card.
+
+const translateX = computed(() => {
+  return centerOffset.value - currentImgIndex.value * slideWidth
+})
+
+function handleLeft() {
+  if (currentImgIndex.value > 0) currentImgIndex.value--
+}
+
+function handleRight() {
+  if (currentImgIndex.value < props.images.length - 1) currentImgIndex.value++
+}
+
+onMounted(() => {
+  containerWidth.value = containerRef.value?.clientWidth || 0
+})
 </script>
 
 <template>
-  <ul class="flex gap-2 overflow-x-auto border-b-2 border-gray-700 bg-gray-300 p-2">
-    <li
-      class="bg-white border-r border-b border-gray-500 relative shrink-0"
-      :style="{
-        width: imageDimensions.width * 4 + 'px',
-        height: imageDimensions.height * 4 + 'px',
-      }"
-      v-for="(img, i) in images"
-      :key="i"
-      data-testid="image-list-item"
+  <div class="flex items-center gap-3 w-full p-3 bg-gray-100 border-b-2 border-gray-700">
+    <button
+      class="p-2 transition-opacity disabled:opacity-40 cursor-pointer disabled:cursor-default"
+      :disabled="currentImgIndex === 0"
+      @click="handleLeft"
     >
-      <ProductImage :src-val="img" :alt-val="`${title} image ${i + 1}`" />
-    </li>
-  </ul>
+      <i class="pi pi-chevron-left text-gray-800"></i>
+    </button>
+
+    <div ref="containerRef" class="w-full overflow-hidden bg-gray-100 border-2 border-gray-400 p-1">
+      <ul
+        class="flex items-center gap-4 p-3 transition-transform duration-500 ease-out will-change-transform"
+        :style="{ transform: `translate3d(${translateX}px, 0, 0)` }"
+      >
+        <li
+          v-for="(img, i) in images"
+          :key="i"
+          class="shrink-0 bg-white border border-gray-700 overflow-hidden transition-transform duration-300"
+          :class="{
+            'scale-110 shadow-lg z-20': i === currentImgIndex,
+            'scale-95 opacity-50': i !== currentImgIndex,
+          }"
+          :style="{
+            width: imageDimensions.width * 4 + 'px',
+            height: imageDimensions.height * 4 + 'px',
+          }"
+          data-testid="image-list-item"
+        >
+          <ProductImage :src-val="img" :alt-val="`${title} image ${i + 1}`" />
+        </li>
+      </ul>
+    </div>
+
+    <button
+      class="p-2 transition-opacity disabled:opacity-40 cursor-pointer disabled:cursor-default"
+      :disabled="currentImgIndex === images.length - 1"
+      @click="handleRight"
+    >
+      <i class="pi pi-chevron-right text-gray-800"></i>
+    </button>
+  </div>
 </template>
